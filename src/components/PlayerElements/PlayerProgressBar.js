003c37ccdca5cc6cc8fdece0fbc800941adb1c81
Bigaston <bigaston1@gmail.com>
2020-11-16 15:02:41 +0100

    fix: load audio on progress bar click

--- src/components/PlayerElements/PlayerProgressBar.js
+++ src/components/PlayerElements/PlayerProgressBar.js
@@ -4,37 +4,47 @@ import playerStore from "../../stores/player";
 import { useRecoilState } from "recoil";
 
 const PlayerProgressBar = ({ playerRef }) => {
-  const [playerState] = useRecoilState(playerStore);
+	const [playerState] = useRecoilState(playerStore);
 
-  const progressbarRef = useRef();
+	const progressbarRef = useRef();
 
-  const { currentTime, duration } = playerState;
+	const { currentTime, duration } = playerState;
 
-  const percent =
-    Math.trunc(((currentTime || 0) / (duration || 1)) * 10000) / 100;
+	const percent =
+		Math.trunc(((currentTime || 0) / (duration || 1)) * 10000) / 100;
 
-  function updateTime(event) {
-    if (playerRef.current && event.target === progressbarRef.current) {
-      const percent = event.nativeEvent.offsetX / event.target.offsetWidth;
-      playerRef.current.currentTime = percent * playerRef.current.duration;
-      console.log(
-        "jump to " + percent.toFixed(2) + " : " + playerRef.current.currentTime
-      );
+	function updateTime(event) {
+		if (playerRef.current && event.target === progressbarRef.current) {
+			const percent = event.nativeEvent.offsetX / event.target.offsetWidth;
+			const player = playerRef.current;
+			const jump = () => {
+				player.currentTime = percent * player.duration;
+				console.log(
+					"jump to " + percent.toFixed(2) + " : " + player.currentTime
+				);
+			};
 
-      playerRef.current.play();
-    } else {
-      debugger;
-    }
-  }
+			if (player.readyState === 1) {
+				jump();
+			} else {
+				player.play().then(() => {
+					player.pause();
+					jump();
+				})
+			}
+		} else {
+			debugger;
+		}
+	}
 
-  return (
-    <div id="progressbar" ref={progressbarRef} onClick={updateTime}>
-      <div
-        id="prog"
-        style={{ width: percent + "%", pointerEvents: "none" }}
-      ></div>
-    </div>
-  );
+	return (
+		<div id="progressbar" ref={progressbarRef} onClick={updateTime}>
+			<div
+				id="prog"
+				style={{ width: percent + "%", pointerEvents: "none" }}
+			></div>
+		</div>
+	);
 };
 
 export default PlayerProgressBar;

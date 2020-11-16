f9dc7e2204f53b947f0e4e85fd76075d9722e7da
Pof Magicfingers <pof@pof.pm>
2020-11-16 15:19:37 +0100

    fix: pass styles to div

--- src/components/EpisodesList/EpisodesList.js
+++ src/components/EpisodesList/EpisodesList.js
@@ -4,9 +4,9 @@ import EpisodesListItem from "./EpisodesListItem";
 
 import "./EpisodesList.css";
 
-const EpisodesList = ({ episodesList, setCurrentEpisode }) => {
+const EpisodesList = ({ episodesList, setCurrentEpisode, style }) => {
   return Array.isArray(episodesList) && episodesList.length > 0 ? (
-    <div className="epList">
+    <div className="epList" style={style}>
       {episodesList.map((ep) => (
         <EpisodesListItem
           key={ep._id}

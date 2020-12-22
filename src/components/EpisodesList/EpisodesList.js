bf5b671f3f275c75e1334b8f58e104d50e92fe61
Pof Magicfingers <pof@pof.pm>
2020-12-22 16:42:03 +0100

    feat: keep list opened in horizontal with list mode

--- src/components/EpisodesList/EpisodesList.js
+++ src/components/EpisodesList/EpisodesList.js
@@ -17,22 +17,26 @@ const EpisodesList = ({
   closeFn,
   open,
 }) => {
-  return Array.isArray(episodesList) && episodesList.length > 0 ? (
+  return (
     <div
       className={classnames(className, styles.list, { [styles.open]: open })}
     >
       <style>:root {`{ --list-opened: ${open ? true : false}; }`}</style>
       <Header className={styles.header} closeFn={closeFn} />
-      <SimpleBar style={{ maxHeight: "100%" }} autoHide={false}>
-        {episodesList.map((ep) => (
-          <EpisodesListItem
-            key={ep._id}
-            episode={ep}
-            setCurrentEpisode={setCurrentEpisode}
-          />
-        ))}
-      </SimpleBar>
+      {episodesList?.loading ? (
+        <div className={styles.loader}></div>
+      ) : Array.isArray(episodesList) && episodesList.length > 0 ? (
+        <SimpleBar style={{ maxHeight: "100%" }} autoHide={false}>
+          {episodesList.map((ep) => (
+            <EpisodesListItem
+              key={ep._id}
+              episode={ep}
+              setCurrentEpisode={setCurrentEpisode}
+            />
+          ))}
+        </SimpleBar>
+      ) : null}
     </div>
-  ) : null;
+  );
 };
 export default EpisodesList;

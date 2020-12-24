a9155634a416f7410189a2886ef5a238ffe62e75
Pof Magicfingers <pof@pof.pm>
2020-12-24 13:45:58 +0100

    feat: add missing translation

--- src/components/EpisodesList/Header.js
+++ src/components/EpisodesList/Header.js
@@ -2,12 +2,16 @@ import React from "react";
 
 import classnames from "classnames";
 
+import { useTranslation } from "react-i18next";
+
 import styles from "./Header.module.scss";
 
 const Header = ({ className, closeFn }) => {
+  const { t } = useTranslation();
+
   return (
     <div className={classnames(className, styles.header)}>
-      Liste des épisodes
+      <span className={styles.title}>{t("episodes_list_title")}</span>
       <div className={styles.close_button} onClick={closeFn}>
         ✖
       </div>
